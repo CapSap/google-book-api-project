3762d5d853956c91ae54ecd6cd9dffc59b05a0c2
CapSap <cshotam@gmail.com>
2022-05-02 11:20:24 +1000

    make function calls in this file

---
+++ script.js
@@ -0,0 +1,15 @@
+import { makeFetchRequest } from "./api.js";
+import { insertDataIntoDom } from "./dom.js";
+
+const formElement = document.querySelector("#bookSearchForm");
+
+formElement.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const myFormData = new FormData(formElement);
+  const userInput = myFormData.get("userInput");
+
+  const data = await makeFetchRequest(userInput);
+
+  insertDataIntoDom(data.items);
+});
